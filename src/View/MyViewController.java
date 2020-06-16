dd0d0e94cda70da27d10c94d4455f8fe3f8430ce
NetanelShaked <48967067+NetanelShaked@users.noreply.github.com>
2020-06-16 16:40:31 +0300

    .

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -38,25 +38,23 @@ public class MyViewController implements IView, Observer, Initializable {
 
     @Override
     public void update(Observable o, Object arg) {
-        if(o instanceof MyViewModel){
-            if(arg instanceof int[][]){
+        if (o instanceof MyViewModel) {
+            if (arg instanceof int[][]) {
                 //GenerateMaze
                 maze = viewModel.getMaze();
                 charCol = viewModel.getCharCol();
                 charRow = viewModel.getCharRow();
-                mazeDisplayer.setMaze(maze,charRow,charCol,viewModel.getGoalRow(),viewModel.getGoalCol());
+                mazeDisplayer.setMaze(maze, charRow, charCol, viewModel.getGoalRow(), viewModel.getGoalCol());
                 mazeDisplayer.draw();
-            }
-            else if(arg instanceof List){
+            } else if (arg instanceof List) {
                 //Solution
                 mazeDisplayer.setSolution((List) arg);
                 mazeDisplayer.draw();
-            }
-            else{
+            } else {
                 //move
-                mazeDisplayer.set_player_position(viewModel.getCharRow(),viewModel.getCharCol());
+                mazeDisplayer.set_player_position(viewModel.getCharRow(), viewModel.getCharCol());
                 mazeDisplayer.draw();
-                if(viewModel.getCharRow()==viewModel.getGoalRow() && viewModel.getCharCol()==viewModel.getGoalCol()){
+                if (viewModel.getCharRow() == viewModel.getGoalRow() && viewModel.getCharCol() == viewModel.getGoalCol()) {
                     //ReachGoal
                 }
             }
@@ -68,42 +66,14 @@ public class MyViewController implements IView, Observer, Initializable {
 
     }
 
-    public void donotWorkAlert(ActionEvent actionEvent) {
-        Alert newalert = new Alert(Alert.AlertType.INFORMATION);
-        newalert.setContentText("we need to start....");
-        newalert.setHeaderText("START!!");
-        newalert.show();
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.exit(0);
-    }
 
-    public void startWorking(ActionEvent actionEvent) {
-        Alert newalert = new Alert(Alert.AlertType.INFORMATION);
-        newalert.setContentText("Lets go Lets GO!!!");
-        newalert.setHeaderText("START!!");
-        ButtonType newbutton = new ButtonType("OK Lets GO ! ");
-        newalert.setTitle("GO-GO-GO-GO-GO");
-        newalert.show();
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.exit(0);
-    }
-
-    public void generateMaze()
-    {
+    public void generateMaze() {
         int rows = Integer.valueOf(textField_mazeRows.getText());
         int cols = Integer.valueOf(textField_mazeColumns.getText());
-        viewModel.generateMaze(rows,cols);
+        viewModel.generateMaze(rows, cols);
     }
 
-    public void solve(){
+    public void solve() {
         viewModel.solve();
     }
 
