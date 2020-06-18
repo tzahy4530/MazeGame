896bd48c4b580d72aa67f8c6f38b7e9db9fa3efa
NetanelShaked <48967067+NetanelShaked@users.noreply.github.com>
2020-06-18 16:25:45 +0300

    update and Save fixed

---
+++ src/View/MainForChecking.java
@@ -0,0 +1,9 @@
+package View;
+
+import java.io.File;
+
+public class MainForChecking {
+    public static void main (String []args){
+        new File(System.getProperty("java.io.tmpdir") + "MazeDataFile.mdf").delete();
+    }
+}
